68d3c0db44a7277c5c16d2a9acdb6a43efc80bed
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-01-30 23:06:38 +0530

    sum using var-arg method

---
+++ src/CoreJava/Main.java
@@ -0,0 +1,41 @@
+package CoreJava;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+ class Main {
+    public static void main(String[] args) {
+         int a=-4521233;
+         int num=Math.abs(a);
+         int size=String.valueOf(num).length();
+        System.out.println(size);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//       if(false)
+//           if(true){
+//              System.out.println("hii");
+//              }
+//       else
+//               System.out.println("raj");
+//
+//       else System.out.println("hello");
+//-----------------------------------------------------------------------------------
+//        while (true)
+//            System.out.println("hii");
+//      System.out.println("hii");
