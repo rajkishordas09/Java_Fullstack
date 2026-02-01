c8dfc1253c34cb768c4713c466ffc7e967e289d9
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-01 20:12:08 +0530

    / operator

---
+++ src/CoreJava/Operator/Q10.java
@@ -0,0 +1,19 @@
+package CoreJava.Operator;
+
+public class Q10 {
+    public static void main(String[] args) {
+
+//        System.out.println(10/0 );
+//        System.out.println(0/0);
+
+        System.out.println(10 / 0.0);
+        System.out.println(-10 / 0.0);
+
+        System.out.println(0 / 0.0);
+        System.out.println(0.0 / 0);
+
+        System.out.println(0 / 12);
+        System.out.println(0.0 / 12);
+
+    }
+}
