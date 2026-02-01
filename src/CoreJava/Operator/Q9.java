86be1a3734b5aa7e72f2b562e7ad2ee7a208072a
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-01 19:55:48 +0530

    increment, decrement operator

---
+++ src/CoreJava/Operator/Q9.java
@@ -0,0 +1,23 @@
+package CoreJava.Operator;
+
+public class Q9 {
+    public static void main(String[] args) {
+        char a = 'W';
+        a++;
+        char b = 65;
+        b++;
+
+        System.out.println("a = " + a + "\n" + "b = " + b);
+
+        int i = 'a';
+        i++;
+        System.out.println("i = " + i);
+
+
+        double d= 11.4;
+        d++;
+        System.out.println("d = " + d);
+
+
+    }
+}
