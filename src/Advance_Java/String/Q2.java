7e7b4b37b1a54b6abc962cf55954a65c600d615f
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-03 21:51:34 +0530

    == and .equals() method of String

---
+++ src/Advance_Java/String/Q2.java
@@ -0,0 +1,16 @@
+package Advance_Java.String;
+
+public class Q2 {
+    public static void main(String[] args) {
+        String s1 = new String("raj");
+        String s2 = new String("raj");
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+
+    }
+
+
+
+}
