ad032d879fae86e397635cb2c5be638432faa9d4
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-05 14:47:43 +0530

    Date Class

---
+++ src/CoreJava/Declaration_and_Access_Modifier/Q1.java
@@ -0,0 +1,9 @@
+package CoreJava.Declaration_and_Access_Modifier;
+import java.util.*;
+//import java.sql.*;
+public class Q1 {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+    }
+}
