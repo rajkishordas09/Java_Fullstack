dd5bb8104a09392c5cc4342a82421c0ed6e43fc0
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-06 17:14:29 +0530

    factors of a number

--- src/CoreJava/Loop/for_loop/Q3.java
+++ src/CoreJava/Loop/for_loop/Q3.java
@@ -2,14 +2,14 @@ package CoreJava.Loop.for_loop;
 
 import java.util.Scanner;
 
-//factorial of a number
+//factors of a number
 public class Q3 {
     public static void main(String[] args) {
         Scanner ac = new Scanner(System.in);
         System.out.println("enter a number : ");
         int n = ac.nextInt();
         System.out.print("factors of " + n+ " are :");
-        for(int i=1;i<=n;i++){
+        for(int i = 1; i <= n; i++){
             if(n % i == 0)
                 System.out.print(i + " ");
         }
