a11b32858cc2ca5ead62f0f515ab83a119149f0e
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-07 23:16:24 +0530

    repeated sum of digits until a single-digit number is obtained.

---
+++ src/CoreJava/Loop/while_loop/Q6.java
@@ -0,0 +1,24 @@
+package CoreJava.Loop.while_loop;
+
+import java.util.Scanner;
+//repeated sum of digits until a single-digit number is obtained. == digitalRoot = 1 + (n - 1) % 9
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner ac = new Scanner(System.in);
+        System.out.println("enter a number : ");
+        int num = ac.nextInt();
+        int sum =num;
+        while (num > 9) {
+            sum = 0;
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if(sum > 9){
+                num = sum ;
+            }
+        }
+        System.out.println("sum is : "+sum);
+
+    }
+}
