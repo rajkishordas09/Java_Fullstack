572def0a4c138085bbc9034aa4648d9f7cf246b7
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-09 17:21:58 +0530

    triangle * pattern

---
+++ src/Dsa_Java/Pattern_Printing/Q7.java
@@ -0,0 +1,19 @@
+package Dsa_Java.Pattern_Printing;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner ac =new Scanner(System.in);
+        System.out.println("enter a value : ");
+        int n = ac.nextInt();
+
+        for (int i = 1; i<= n ; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
