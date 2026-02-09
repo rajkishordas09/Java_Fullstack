2f6e72bb386a19259b1f9cc126062e3822b21f3f
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-09 17:58:31 +0530

    reverse triangle number pattern

---
+++ src/Dsa_Java/Pattern_Printing/Q9.java
@@ -0,0 +1,29 @@
+package Dsa_Java.Pattern_Printing;
+
+import java.util.Scanner;
+
+public class Q9 {
+    public static void main(String[] args) {
+        Scanner ac =new Scanner(System.in);
+        System.out.println("enter a value : ");
+        int n = ac.nextInt();
+
+        for (int i = 1; i<= n ; i++){
+            int a = 5;
+            for (int j = n; j >= i; j--){
+                System.out.print(a + " ");
+                a--;
+            }
+            System.out.println();
+        }
+
+    }
+}
+//
+//enter a value :
+//        5
+//        5 4 3 2 1
+//        5 4 3 2
+//        5 4 3
+//        5 4
+//        5
