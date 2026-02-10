9a6af75870659de27cfe6ca46803cf2a75465d6c
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-10 22:22:19 +0530

    print number at diagonal pattern

--- src/Dsa_Java/Pattern_Printing/Q13.java
+++ src/Dsa_Java/Pattern_Printing/Q13.java
@@ -7,18 +7,18 @@ public class Q13 {
         Scanner ac =new Scanner(System.in);
         System.out.println("enter a value : ");
         int n = ac.nextInt();
-        int a = 1;
+//        int a = 1;
         for (int i = 1; i<= n ; i++){
 
             for (int j = 1; j <= n; j++){
                 if(i == j || (i + j == n + 1)) {
-                    if (i == a || (i + j == n + 1))
-                        System.out.print(a);
+//                    if (i == a || (i + j == n + 1))
+                        System.out.print(i);
                 }
 
                 else System.out.print(" ");
             }
-            a++;
+//            a++;
             System.out.println();
         }
 
