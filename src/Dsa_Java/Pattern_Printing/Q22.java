2af551689fd1882eaebe479e5fa90fe9b02dff8f
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-11 20:39:12 +0530

    hollow dimond * pattern

---
+++ src/Dsa_Java/Pattern_Printing/Q22.java
@@ -0,0 +1,45 @@
+package Dsa_Java.Pattern_Printing;
+
+import java.util.Scanner;
+
+public class Q22 {
+    public static void main(String[] args) {
+        Scanner ac =new Scanner(System.in);
+        System.out.println("enter a value : ");
+        int n = ac.nextInt();
+        int space = n / 2;
+        int star = 1;
+        for (int i = 1; i<= n ; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= star; j++) {
+               if( j == 1 || j == star || i == n/2 )
+                System.out.print("*");
+               else System.out.print(" ");
+            }
+            System.out.println();
+            if(i < n/2){
+                space--;
+                star +=2;
+
+            }
+            else {
+                space++;
+                star -=2;
+            }
+        }
+    }
+}
+
+//10
+//            *
+//           * *
+//          *   *
+//         *     *
+//        *********
+//         *     *
+//          *   *
+//           * *
+//            *
+
