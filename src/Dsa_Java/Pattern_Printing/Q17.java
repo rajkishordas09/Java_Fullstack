ee4dfebb756e981048876a78b514052a90adaaf0
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-11 20:10:05 +0530

    print * pattern

---
+++ src/Dsa_Java/Pattern_Printing/Q17.java
@@ -0,0 +1,33 @@
+package Dsa_Java.Pattern_Printing;
+
+import java.util.Scanner;
+
+public class Q17 {
+    public static void main(String[] args) {
+        Scanner ac =new Scanner(System.in);
+        System.out.println("enter a value : ");
+        int n = ac.nextInt();
+        int space = n - 1;
+        int star = 1;
+        for (int i = 1; i<= n ; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= star; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+            space--;
+            star ++;
+        }
+    }
+}
+
+//enter a value :
+//        5
+//            *
+//           **
+//          ***
+//         ****
+//        *****
