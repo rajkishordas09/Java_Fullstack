84081dfe44f156a666808b9d6bff569fb6fbc3ef
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-12 19:44:29 +0530

    hollow dimond * pattern

---
+++ src/Dsa_Java/Pattern_Printing/Q23.java
@@ -0,0 +1,35 @@
+package Dsa_Java.Pattern_Printing;
+
+import java.util.Scanner;
+
+//hollow diamond
+public class Q23 {
+    public static void main(String[] args) {
+        Scanner ac = new Scanner(System.in);
+        System.out.println("enter a number : ");
+        int n = ac.nextInt();
+        int space = n / 2;
+        int star = 1;
+        for (int i = 1;i <= n; i++){
+            for (int  j = 1; j <= space;j++ ){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= star;j++){
+                if (j == 1 || j == star){
+                    System.out.print("*");
+                }
+                else System.out.print(" ");
+            }
+            System.out.println();
+            if (i <= n/2){
+                space --;
+                star+=2;
+            }
+            else {
+                space++;
+                star -=2;
+            }
+        }
+    }
+    
+}
