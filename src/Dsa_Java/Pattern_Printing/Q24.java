d3ad34291a5d1761176895e3662e21e9750f0805
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-12 19:53:40 +0530

    hollow dimond * pattern with  diagonal

---
+++ src/Dsa_Java/Pattern_Printing/Q24.java
@@ -0,0 +1,33 @@
+package Dsa_Java.Pattern_Printing;
+
+import java.util.Scanner;
+
+public class Q24 {
+    public static void main(String[] args) {
+        Scanner ac = new Scanner(System.in);
+        System.out.println("enter a number : ");
+        int n = ac.nextInt();
+        int space = n/2;
+        int star = 1;
+        for (int i = 1; i<= n;i++){
+            for (int j =1;j <= space;j++){
+                System.out.print(" ");
+            }
+            for (int  j = 1;j <= star; j++){
+                if(j == 1 || j == star || i == n/2+1 || j == star/2+1){
+                    System.out.print("*");
+                }
+                else System.out.print(" ");
+            }
+            System.out.println();
+            if(i <= n/2){
+                space--;
+                star+=2;
+            }
+            else {
+                space++;
+                star-=2;
+            }
+        }
+    }
+}
