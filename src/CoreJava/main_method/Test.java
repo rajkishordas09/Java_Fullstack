9ffd1802e823690ef422391d125ac3d3b0da2c7f
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-16 20:59:32 +0530

    main method

---
+++ src/CoreJava/main_method/Test.java
@@ -0,0 +1,13 @@
+package CoreJava.main_method;
+
+ class Test {
+    public static void main(String[] args){
+        System.out.println("parent class");
+    }
+}
+class A extends Test{
+    public static void main(String[] args){
+        System.out.println("child class");
+    }
+}
+
