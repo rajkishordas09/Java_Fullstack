6cde7fae78a38bf8d721620ced68345664a25223
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-21 22:28:26 +0530

    default modifier of static method

---
+++ src/CoreJava/Methods/pkg1/Q1.java
@@ -0,0 +1,7 @@
+package CoreJava.Methods.pkg1;
+
+ class Q1 {
+     static void m(){
+         System.out.println("hello");
+     }
+ }
---
+++ src/CoreJava/Methods/pkg1/Q2.java
@@ -0,0 +1,10 @@
+package CoreJava.Methods.pkg1;
+
+//import static CoreJava.Methods.pkg1.Q1.m;
+
+class Q2 {
+     public static void main(String[] args) {
+//         Q1 q = new Q1();
+         Q1.m();   // inside same class direct we can use m() but different class use className.methodName
+     }
+}
