1e1d6b3deb23f21ae47a0d199963fba745db472c
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-21 22:30:13 +0530

    public modifier of static nd non-static method with different pkg

---
+++ src/CoreJava/Methods/pkg1/Q5.java
@@ -0,0 +1,7 @@
+package CoreJava.Methods.pkg1;
+
+public class Q5 {
+    public static void m(){
+        System.out.println("RAJ");
+    }
+}
---
+++ src/CoreJava/Methods/pkg1/Q6.java
@@ -0,0 +1,7 @@
+package CoreJava.Methods.pkg1;
+
+public class Q6 {
+    public void m(){
+        System.out.println("kishor");
+    }
+}
