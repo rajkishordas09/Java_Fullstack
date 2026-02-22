a5ad70c6c8c0c6d6899774244bb024d384daf9c6
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-22 20:57:14 +0530

    final modifier

---
+++ src/CoreJava/Modifiers/Final_Modifier/Q3.java
@@ -0,0 +1,12 @@
+package CoreJava.Modifiers.Final_Modifier;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Q2 q = new Q2();
+//        q.a = 300;
+        System.out.println(q.a);
+
+//        Q2.b = 345;
+        System.out.println(Q2.b);
+    }
+}
