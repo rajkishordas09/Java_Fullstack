89ed6e74731d834bf8b8c566a46b46c85c4982dd
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-22 20:56:25 +0530

    import statment

--- src/CoreJava/Import/pkg3/Q2.java
+++ src/CoreJava/Import/pkg3/Q2.java
@@ -1,11 +1,11 @@
-package CoreJava.Declaration_and_Access_Modifier.pkg3;
+package CoreJava.Import.pkg3;
 //import java.util.Date;
-import java.util.*;
 
 public class Q2 {
     public static void main(String[] args) {
         Date a = new Date();
         System.out.println(a.getClass().getName());
+        System.out.println(a);
         //here we create reference because getClass() is non static method
         //explicit class import > Same class name present in current package > implicit class import
     }
