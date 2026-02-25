08fcf0e036551e5596e3db499e5b271a3c70ac9c
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-25 19:41:46 +0530

    final  modifier  instance variable

--- src/CoreJava/Modifiers/Final_Modifier/Variables/Final_instance_Variables/Q1.java
+++ src/CoreJava/Modifiers/Final_Modifier/Variables/Final_instance_Variables/Q1.java
@@ -2,9 +2,19 @@ package CoreJava.Modifiers.Final_Modifier.Variables.Final_instance_Variables;
 
 public class Q1 {
     final int a = 20;
+
+    void m1(){
+        System.out.println(a);
+    }
+    static void m2(){
+//        System.out.println(a);  //error
+    }
+
     public static void main(String[] args) {
         Q1 q = new Q1();
 //        q.a= 20;
         System.out.println(q.a);
+
+        q.m1();
     }
 }
