e2cc67652332f71f8b4e41506a45e366cc261559
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-25 19:21:39 +0530

    final  modifier for static variable

---
+++ src/CoreJava/Modifiers/Final_Modifier/Variables/Final_Static_Variables/Q1.java
@@ -0,0 +1,10 @@
+package CoreJava.Modifiers.Final_Modifier.Variables.Final_Static_Variables;
+
+public class Q1 {
+    final static int a = 20;
+    public static void main(String[] args) {
+
+//       Q1.a= 20;
+        System.out.println(Q1.a);
+    }
+}
