991ffa4168270379f7254364e9fce6f8ea8996e3
Raj kishor Das <rajkishordas.04150@gmail.com>
2026-02-25 19:21:52 +0530

    final  modifier

---
+++ src/CoreJava/Modifiers/Final_Modifier/Method_and_Class/pkg2/P3.java
@@ -0,0 +1,9 @@
+package CoreJava.Modifiers.Final_Modifier.Method_and_Class.pkg2;
+import CoreJava.Modifiers.Final_Modifier.Method_and_Class.pkg1.Q1;
+public class P3 {
+    public static void main(String[] args) {
+        Q1 q = new Q1();
+        q.m1();
+//        Q1.m2();
+    }
+}
